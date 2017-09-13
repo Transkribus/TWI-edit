504948bddc15ea5f07ffe4754ea4682742f4993a
jnphilipp <mail@jnphilipp.org>
2017-09-13 14:36:20 +0300

    Fix btn-toggle.

--- static/js/btn-toggle.js
+++ static/js/btn-toggle.js
@@ -1,31 +1,29 @@
 $(".btn-toggle").click(function(e) {
-    if ( !$(this).hasClass("btn-primary") ) {
-        if ( $(this).closest(".btn-group").find(".btn-primary").size() > 0 ) {
-            $(this).closest(".btn-group").find(".active").toggleClass("btn-primary");
-            $(this).closest(".btn-group").find(".active").toggleClass("btn-default");
-            $(this).closest(".btn-group").find(e.target).toggleClass("btn-default");
-            $(this).closest(".btn-group").find(e.target).toggleClass("btn-primary");
-        }
-        if ( $(this).closest(".btn-group").find(".btn-danger").size() > 0 ) {
-            $(this).closest(".btn-group").find(".active").toggleClass("btn-danger");
-            $(this).closest(".btn-group").find(".active").toggleClass("btn-default");
-            $(this).closest(".btn-group").find(e.target).toggleClass("btn-default");
-            $(this).closest(".btn-group").find(e.target).toggleClass("btn-danger");
-        }
-        if ( $(this).closest(".btn-group").find(".btn-success").size() > 0 ) {
-            $(this).closest(".btn-group").find(".active").toggleClass("btn-success");
-            $(this).closest(".btn-group").find(".active").toggleClass("btn-default");
-            $(this).closest(".btn-group").find(e.target).toggleClass("btn-default");
-            $(this).closest(".btn-group").find(e.target).toggleClass("btn-success");
-        }
-        if ( $(this).closest(".btn-group").find(".btn-info").size() > 0 ) {
-            $(this).closest(".btn-group").find(".active").toggleClass("btn-info");
-            $(this).closest(".btn-group").find(".active").toggleClass("btn-default");
-            $(this).closest(".btn-group").find(e.target).toggleClass("btn-default");
-            $(this).closest(".btn-group").find(e.target).toggleClass("btn-info");
-        }
-
-        $(this).closest(".btn-group").find(".active").toggleClass("active");
-        $(this).closest(".btn-group").find(e.target).toggleClass("active");
+    if ( $(this).closest(".btn-group").find(".btn-toggle.btn-primary").size() > 0 ) {
+        $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("btn-primary");
+        $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("btn-default");
+        $(this).closest(".btn-group").find(e.target).toggleClass("btn-default");
+        $(this).closest(".btn-group").find(e.target).toggleClass("btn-primary");
     }
+    if ( $(this).closest(".btn-group").find(".btn-toggle.btn-danger").size() > 0 ) {
+        $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("btn-danger");
+        $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("btn-default");
+        $(this).closest(".btn-group").find(e.target).toggleClass("btn-default");
+        $(this).closest(".btn-group").find(e.target).toggleClass("btn-danger");
+    }
+    if ( $(this).closest(".btn-group").find(".btn-toggle.btn-success").size() > 0 ) {
+        $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("btn-success");
+        $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("btn-default");
+        $(this).closest(".btn-group").find(e.target).toggleClass("btn-default");
+        $(this).closest(".btn-group").find(e.target).toggleClass("btn-success");
+    }
+    if ( $(this).closest(".btn-group").find(".btn-toggle.btn-info").size() > 0 ) {
+        $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("btn-info");
+        $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("btn-default");
+        $(this).closest(".btn-group").find(e.target).toggleClass("btn-default");
+        $(this).closest(".btn-group").find(e.target).toggleClass("btn-info");
+    }
+
+    $(this).closest(".btn-group").find(".btn-toggle.active").toggleClass("active");
+    $(this).closest(".btn-group").find(e.target).toggleClass("active");
 });
