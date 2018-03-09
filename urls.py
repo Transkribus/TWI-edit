5ff61572f408b2a04ee861bace2fe88758d590c5
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2018-03-09 18:17:54 +0000

    make links work to and from edit for both master and refactor branches

--- urls.py
+++ urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     url(r'^([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='correct'),
     url(r'^([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='correct'),
     #url(r'transcribe/([0-9]+)/([0-9]+)/([0-9]+)$', views.transcribe, name='transcribe'),
+
+    url(r'^([0-9]+)/([0-9]+)$', views.correct, name='document-view'),
+    #allow edit of current transcript with no transcriptId, also allow omission of the page number (needed at least to get the path for some JavaScript stuff)
+    url(r'^([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='document-view'),
+    url(r'^([0-9]+)/([0-9]+)/([0-9]+)/([0-9]+)$', views.correct, name='document-view'),
+ 
 ]
